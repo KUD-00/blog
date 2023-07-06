6ab78424ee0c94c6a385544c5f47f3583115f099
KUD <kasa7qi@gmail.com>
2023-07-06 14:20:07 +0900

    update posts

--- app/(marketing)/project/page.tsx
+++ app/(marketing)/project/page.tsx
@@ -57,15 +57,16 @@ export default async function Project() {
             <div className="stats shadow">
               <div className="stat">
                 <div className="stat-title">阅读practical linux system</div>
-                <div className="stat-value">13/15</div>
+                <div className="stat-value">15/15</div>
                 <div className="stat-desc">多了解一点linux</div>
-                <progress className="progress w-56" value={58} max="100"></progress>
+                <progress className="progress w-56" value={100} max="100"></progress>
               </div>
             </div>
           </div>
           <div className="collapse-content">
             <p>2023/6/25:还是有些新东西的</p>
             <p>2023/7/01:although i begin to use nixos, this book still gives me some inspiration</p>
+            <p>2023/7/06:finally, done</p>
           </div>
         </div>
         <div className="collapse bg-base-200">
@@ -82,6 +83,22 @@ export default async function Project() {
           </div>
           <div className="collapse-content">
             <p>2023/7/02:need to know more. to find jobs i think</p>
+          </div>
+        </div>
+        <div className="collapse bg-base-200">
+          <input type="checkbox" />
+          <div className="collapse-title text-xl font-medium">
+            <div className="stats shadow">
+              <div className="stat">
+                <div className="stat-title">阅读amazon web services</div>
+                <div className="stat-value">3/18</div>
+                <div className="stat-desc">boss of cloud</div>
+                <progress className="progress w-56" value={10} max="100"></progress>
+              </div>
+            </div>
+          </div>
+          <div className="collapse-content">
+            <p>2023/7/06: to find jobs, to build things using S3 object store</p>
           </div>
         </div>
       </div>
