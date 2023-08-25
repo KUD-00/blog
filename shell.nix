467e7717fe3dd5ee568551c95aa086c90a83ea4f
KUD <kasa7qi@gmail.com>
2023-08-25 15:33:27 +0800

    add shell.nix

--- shell.nix
+++ shell.nix
@@ -5,15 +5,9 @@ let
 in
 pkgs.mkShell (with pkgs; {
   buildInputs = [
+    git
+    gh
     nodejs
-    openssl
+    nodePackages_latest.pnpm
   ];
-
-  shellHook = ''PATH="$PWD/node_modules/.bin:$PATH"'';
-
-  PRISMA_MIGRATION_ENGINE_BINARY = "${prisma-engines}/bin/migration-engine";
-  PRISMA_QUERY_ENGINE_BINARY = "${prisma-engines}/bin/query-engine";
-  PRISMA_QUERY_ENGINE_LIBRARY = "${prisma-engines}/lib/libquery_engine.node";
-  PRISMA_INTROSPECTION_ENGINE_BINARY = "${prisma-engines}/bin/introspection-engine";
-  PRISMA_FMT_BINARY = "${prisma-engines}/bin/prisma-fmt";
 })
