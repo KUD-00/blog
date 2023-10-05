6acd3db64627f5d489d7e9103d22f2474ef1293e
KUD <kasa7qi@gmail.com>
2023-10-05 19:45:23 +0900

    update dependencies

--- shell.nix
+++ shell.nix
@@ -9,5 +9,13 @@ pkgs.mkShell (with pkgs; {
     gh
     nodejs
     nodePackages_latest.pnpm
+    openssl
   ];
+  shellHook = ''PATH="$PWD/node_modules/.bin:$PATH"'';
+
+  PRISMA_MIGRATION_ENGINE_BINARY = "${prisma-engines}/bin/migration-engine";
+  PRISMA_QUERY_ENGINE_BINARY = "${prisma-engines}/bin/query-engine";
+  PRISMA_QUERY_ENGINE_LIBRARY = "${prisma-engines}/lib/libquery_engine.node";
+  PRISMA_INTROSPECTION_ENGINE_BINARY = "${prisma-engines}/bin/introspection-engine";
+  PRISMA_FMT_BINARY = "${prisma-engines}/bin/prisma-fmt";
 })
